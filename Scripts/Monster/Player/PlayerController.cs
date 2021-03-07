b66bfe994e250986390920410ebad40ed133039a
Daniel Silva <dodosilva51@gmail.com>
2021-03-07 19:32:55 -0300

    Added first tileset to the overworld itself
    Implemented grid like movement (horizontal and vertical only move)
    
    Added sprite changer (both for idle and walk movement) to the player
    
    .gitignore to the .meta files
    
    LICENSE information and first .git rituals
    
    Added some assets to have a 'database' for arts and ui things

---
+++ Scripts/Monster/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Vector2 input;
+    private Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isMoving) {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            // To not move in diagonal
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero) {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos) {
+        isMoving = true;
+        animator.SetBool("isMoving", isMoving);
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, 
+                moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
+        animator.SetBool("isMoving", isMoving);
+    }
+}
