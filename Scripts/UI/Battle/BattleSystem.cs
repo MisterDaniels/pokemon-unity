98558d45c0419aa61442e1d71c6d6afd3cfcf8dc
Daniel Silva <dodosilva51@gmail.com>
2021-03-12 20:38:25 -0300

    Added class to battleScene dialog to handle dialogBox
    Added enums to state of battleScene, handler to player choose action

--- Scripts/UI/Battle/BattleSystem.cs
+++ Scripts/UI/Battle/BattleSystem.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using Pokemon;
 
+public enum BattleState {
+    Start,
+    PlayerAction,
+    PlayerMove,
+    EnemyMove,
+    Busy
+}
+
 namespace UI.Battle {
 
     public class BattleSystem : MonoBehaviour {
@@ -12,17 +20,60 @@ namespace UI.Battle {
         [SerializeField] BattleUnit enemyUnit;
         [SerializeField] BattleHud playerHud;
         [SerializeField] BattleHud enemyHud;
+        [SerializeField] BattleDialog dialogBox;
+
+        BattleState state;
+        int currentAction;
 
         private void Start() {
-            SetupBattle();
+            StartCoroutine(SetupBattle());
         }
 
-        public void setupBattle() {
+        public IEnumerator setupBattle() {
             playerUnit.Setup();
             playerHud.SetData(playerUnit.Pokemon);
 
             enemyUnit.Setup();
             enemyHud.SetData(enemyUnit.Pokemon);
+
+            yield return dialogBox.TypeDialog($"A wild { enemyUnity.Pokemon.Base.Name } appeared");
+            yield return new WaitForSeconds(1f);
+
+            PlayerAction();
+        }
+
+        private void PlayerAction() {
+            state = BattleState.PlayerAction;
+            StartCoroutine(dialogBox.TypeDialog('Choose and action'));
+            dialogBox.EnableActionSelector(true);
+        }
+
+        private void Update() {
+            if (state == BattleState.PlayerAction) {
+                HandleActionSelection();
+            }
+        }
+
+        private void HandleActionSelection() {
+            if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                if (currentAction < 1) {
+                    ++currentAction;
+                } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                    if (currentAction > 0) {
+                        --currentAction;
+                    }
+                }
+            }
+
+            dialogBox.UpdateActionSelection(currentAction);
+
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                if (currentAction == 0) {
+                    // Fight
+                } else if (currentAction == 1) {
+                    // Run
+                }
+            }
         }
 
     }
